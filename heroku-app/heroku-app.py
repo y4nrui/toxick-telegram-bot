0d38a4ae8c972b04ca4f431ba4312657c97c1f60
Yan Rui <54534120+y4nrui@users.noreply.github.com>
2021-06-27 13:51:49 +0800

    push main bot
    main code base for heroku telegram bot

---
+++ heroku-app/heroku-app.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+Simple Bot to reply to Telegram messages.
+
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+import logging
+import random
+import os
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+# SECRET_TOKEN = "1856799840:AAHCwlC5PGKQj9aZacEe42nq_nAJv9RjC90"
+SECRET_TOKEN = os.getenv("SECRET_TOKEN")
+PORT = int(os.environ.get('PORT', 8443))
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+possible_replies = [
+        "fk ur mother",
+        "stfu no one cares",
+        "how about you go and fuck yourself?",
+        "fuck your mom and your whole family",
+        "your mom pass away"
+]
+
+def echo(update, context):
+    """Echo the user message."""
+
+    # update.message.reply_text(update.message.text)
+    
+
+    random_index = random.randint(0,len(possible_replies)-1)
+    if "?" in update.message.text:
+        update.message.reply_text(possible_replies[2])
+    else:
+        update.message.reply_text(possible_replies[random_index])
+    
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(SECRET_TOKEN, use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=SECRET_TOKEN,
+                          webhook_url='https://serene-oasis-90115.herokuapp.com/' + SECRET_TOKEN)
+    
+    # updater.bot.setWebhook('https://serene-oasis-90115.herokuapp.com/' + SECRET_TOKEN)
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()

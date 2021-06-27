24fa93d79904befe7d518cd30477b859c6d4e65d
Yieh Yuheng <54618128+yiehyuheng@users.noreply.github.com>
2021-06-27 14:54:00 +0800

    Processing functions

--- processing/preprocessing.py
+++ processing/preprocessing.py
@@ -7,3 +7,7 @@ def overall_cleantext(telegram_message):
     telegram_message = expand_abbr(telegram_message)
     telegram_message = remove_punctuation(telegram_message)
     telegram_message = remove_stopwords(telegram_message)
+    loaded_tokenizer_model = pickle.load(open(tokenizer_file, 'rb'))
+    telegram_message = loaded_tokenizer_model.texts_to_sequences([test_text])
+    telegram_message = pad_sequences(telegram_message, padding = 'post', maxlen = MAX_SEQUENCE_LENGTH)
+    return telegram_message
